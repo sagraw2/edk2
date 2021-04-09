b197d519af0b39d97d5b6a0cea4c086a4c6a0fce
Sachin Agrawal <sachin.agrawal@intel.com>
2021-04-09 15:14:04 -0700

    CryptoPkg: BaseCryptLib: Add RSA PSS verify support
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3314
    
    This patch uses Openssl's EVP API's to perform RSASSA-PSS verification
    of a binary blob.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Xiaoyu Lu <xiaoyux.lu@intel.com>
    Cc: Guomin Jiang <guomin.jiang@intel.com>
    
    Signed-off-by: Sachin Agrawal <sachin.agrawal@intel.com>

--- CryptoPkg/Include/Library/BaseCryptLib.h
+++ CryptoPkg/Include/Library/BaseCryptLib.h
@@ -1364,6 +1364,33 @@ RsaPkcs1Verify (
   );
 
 /**
+  Verifies the RSA signature with RSASSA-PSS signature scheme defined in RFC 8017.
+  Implementation determines salt length automatically from the signature encoding.
+  Mask generation function is the same as the message digest algorithm.
+
+  @param[in]  RsaContext      Pointer to RSA context for signature verification.
+  @param[in]  Message         Pointer to octet message to be verified.
+  @param[in]  MsgSize         Size of the message in bytes.
+  @param[in]  Signature       Pointer to RSASSA-PSS signature to be verified.
+  @param[in]  SigSize         Size of signature in bytes.
+  @param[in]  DigestLen       Length of digest for RSA operation.
+
+  @retval  TRUE   Valid signature encoded in RSASSA-PSS.
+  @retval  FALSE  Invalid signature or invalid RSA context.
+
+**/
+BOOLEAN
+EFIAPI
+RsaPssVerify (
+  IN  VOID         *RsaContext,
+  IN  CONST UINT8  *Message,
+  IN  UINTN        MsgSize,
+  IN  CONST UINT8  *Signature,
+  IN  UINTN        SigSize,
+  IN  UINT16       DigestLen
+  );
+
+/**
   Retrieve the RSA Private Key from the password-protected PEM key data.
 
   If PemData is NULL, then return FALSE.
